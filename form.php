40fe8b1ec0fed16ae52f366536a918d1d07cc6c5
Scroph <scroph@localhost>
2012-11-29 21:08:46 +0000

    Wrote some rudimentary documentation

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <form method = "post" action = "test.php" enctype = "multipart/form-data">
 	<label>
 		Fichier à uploader : <input type = "file" name = "fichier" />
-    </label>
+	</label>
 	<input type = "submit" value = "Valider" />
 </form>
--- test.php
+++ test.php
@@ -1,19 +1,16 @@
 <?php
 include 'autoload.php';
 
-echo '<pre>';
-print_r($_FILES);
-echo '</pre>';
-
 $uploader = new Uploader\Uploader;
 
 try
 {
-	$uploader   ->setLandingPath(__DIR__.DS.'files/')
+	$uploader   ->setLandingPath(__DIR__.DS.'files')
 				->addFilter(new Uploader\Filters\SizeFilter(2 * 1024 * 1024))
 				->addFilter(new Uploader\Filters\ExtensionFilter(array('jpg', 'png', 'jpeg', 'pdf')));
 
-	$uploader->process('fichier', $_FILES);
+	$path = $uploader->process('fichier', $_FILES);
+	echo 'Le fichier figure dans le chemin suivant : '.$path;
 }
 catch(Uploader\Exceptions\UploaderException $e)
 {
