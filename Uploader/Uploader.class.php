1c02aec9652256d71e2e650af26c8ef12b786fa4
Scroph <scroph@localhost>
2012-11-29 20:50:18 +0000

    Wrote some basic documentation

--- Uploader/Uploader.class.php
+++ Uploader/Uploader.class.php
@@ -9,6 +9,11 @@ class Uploader
 	protected $_filters;
 	protected $_errors;
 
+	/*
+     * @param string $path
+	 * @throws UploaderException
+	 * @return Uploader $this
+	 */
 	public function setLandingPath($path)
 	{
 		if(!is_writable($path))
@@ -19,12 +24,22 @@ class Uploader
 		return $this;
 	}
 
+	/*
+	 * @param IFilter $filter
+	 * @return Uploader $this
+	 */
 	public function addFilter(IFilter $filter)
 	{
 		$this->_filters[] = $filter;
 		return $this;
 	}
 
+	/*
+	 * @param string $file
+	 * @param array $_FILES
+	 * @throws UploaderException
+	 * @return string $path
+	 */
 	public function process($file, array $files)
 	{
 		if(!array_key_exists($file, $files) || $files[$file]['error'] > 0)
